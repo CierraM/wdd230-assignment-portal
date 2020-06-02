a12da7cac7a5833fd12caefcb885fd5fd9ba6ac1
CierraM <4cibear@gmail.com>
2020-06-02 15:41:58 -0600

    worked on intersection observer. Blur not working now.

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,28 +1,69 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {
+//     image.removeAttribute('data-src');
+//   };
+// };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
 
 
 
-  let options = {
-      root: document.querySelector('#scrollArea'),
-      rootMargin: '0px',
-      threshold: 1.0
+//   let options = {
+//       root: document.querySelector('#scrollArea'),
+//       rootMargin: '0px',
+//       threshold: 1
+//   }
+
+//   let observer = new IntersectionObserver(callback, options);
+
+//   let target = document.querySelector('data-src');
+//   observer.observe(target);
+
+//   let callback = (entries, observer) => {
+//       entries.forEach(entry =>{})
+//   }
+
+
+////This is the practice we did in class
+
+////entries = what is being observed
+
+////square brackets refer to an attribute
+const images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img) {
+  const source = img.getAttribute('data-src');
+  if (!source) {
+    return;
   }
 
-  let observer = new IntersectionObserver(callback, options);
+  img.src = source;
+}
 
-  let target = document.querySelector('data-src');
-  observer.observe(target);
+const options = {
+  threshold: 0
 
-  let callback = (entries, observer) => {
-      entries.forEach(entry =>{})
-  }
+}
+
+const io = new IntersectionObserver(
+  (entries, io) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        preloadImage(entry.target); //target means which image you are intersecting
+        io.unobserve(entry.target); //stop observing
+      }
+    });
+
+  }, options
+
+)
+
+images.forEach(image => {
+  io.observe(image);
+})
