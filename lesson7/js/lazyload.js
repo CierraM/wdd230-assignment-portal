5f78554b0f759c866ddd51da13450d1d88ae0ba7
CierraM <4cibear@gmail.com>
2020-06-03 16:40:06 -0600

    fixed blurring problem

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,38 +1,3 @@
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
-// const loadImages = (image) => {
-//   image.setAttribute('src', image.getAttribute('data-src'));
-//   image.onload = () => {
-//     image.removeAttribute('data-src');
-//   };
-// };
-
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-//   });
-
-
-
-//   let options = {
-//       root: document.querySelector('#scrollArea'),
-//       rootMargin: '0px',
-//       threshold: 1
-//   }
-
-//   let observer = new IntersectionObserver(callback, options);
-
-//   let target = document.querySelector('data-src');
-//   observer.observe(target);
-
-//   let callback = (entries, observer) => {
-//       entries.forEach(entry =>{})
-//   }
-
-
-////This is the practice we did in class
-
-////entries = what is being observed
-
-////square brackets refer to an attribute
 const images = document.querySelectorAll('[data-src]');
 
 function preloadImage(img) {
@@ -41,28 +6,30 @@ function preloadImage(img) {
     return;
   }
 
+  img.onload = () => {
+    img.removeAttribute('data-src')
+  }
+
   img.src = source;
 }
 
 const options = {
-  threshold: 0
+  threshold: .5
+}
+
+const callback = (entries, io) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target); //target means which image you are intersecting
+      io.unobserve(entry.target); //stop observing
+    }
+  });
 
 }
 
-const io = new IntersectionObserver(
-  (entries, io) => {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        preloadImage(entry.target); //target means which image you are intersecting
-        io.unobserve(entry.target); //stop observing
-      }
-    });
-
-  }, options
-
-)
+const io = new IntersectionObserver(callback, options)
 
 images.forEach(image => {
   io.observe(image);
